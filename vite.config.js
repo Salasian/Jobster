15a0ccfd95afb36918b7a85e7574f5a0cad896b7
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2023-09-27 20:22:28 -0700

    augmented the chunksize

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   plugins: [react()],
-})
+});
